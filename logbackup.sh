8a7b31d491a90ade30cf6ceea52168932841da72
tobyarnett <39145409+tobyarnett@users.noreply.github.com>
2023-05-21 00:24:22 -0500

    Update logbackup.sh

--- logbackup.sh
+++ logbackup.sh
@@ -14,50 +14,43 @@
 # Set color variables
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
-blue=$(tput setaf 4)
-reverse=$(tput rev)
+red=$(tput setaf 1)
 bold=$(tput bold)
 reset=$(tput sgr0)
 
-# Prompt for user input
-echo "${blue}What is the network share drive? (i.e. //192.168.1.10/logs):${reset}"
+echo "${yellow}What is the network share drive? (i.e. //192.168.1.10/logs):${reset}"
 read network_drive
-
-echo "${blue}What is the username for the network share?${reset}"
-read network_username
-
-echo "${blue}What is the password for the network share?${reset}"
-read -s network_password
-
-echo "${blue}What is the name of the local folder to use:${reset}"
+echo "${yellow}What is the name of the local folder to use:${reset}"
 read local_folder
-local_folder=${local_folder:-BackupLogs}
 
-# Create the local folder if it doesn't exist
-mkdir -p /var/log/$local_folder
+# Create the mount point if it doesn't exist
+if [ ! -d "/mnt/$local_folder" ]; then
+    echo "${yellow}Creating mount point /mnt/$local_folder...${reset}"
+    sudo mkdir /mnt/$local_folder
+fi
 
 # Mount the network drive
-echo "Mounting the network drive..."
-mount_point="/mnt/network_drive"
-mkdir -p $mount_point
-mount -t cifs $network_drive $mount_point -o username=$network_username,password=$network_password
-if [ $? -ne 0 ]; then
-  echo "${red}Failed to mount the network drive. Please check your settings and try again.${reset}"
-  exit 1
+echo "${yellow}Mounting the network drive...${reset}"
+mount_output=$(sudo mount -t cifs $network_drive /mnt/$local_folder -o username=guest,vers=1.0 2>&1)
+
+if [ $? -eq 0 ]; then
+    echo "${green}Network drive mounted successfully.${reset}"
+else
+    echo "${red}Failed to mount the network drive. Please check your settings and try again.${reset}"
+    echo "Error details: $mount_output"
+    exit 1
 fi
 
-# Add the network drive to /etc/fstab if it's not already there
-if ! grep -q "$network_drive" /etc/fstab; then
-  echo "$network_drive $mount_point cifs username=$network_username,password=$network_password 0 0" >> /etc/fstab
-  systemctl daemon-reload
+# Add the mount to /etc/fstab if it's not already there
+if ! grep -q "$network_drive /mnt/$local_folder" /etc/fstab; then
+    echo "${yellow}Adding the mount to /etc/fstab...${reset}"
+    echo "$network_drive /mnt/$local_folder cifs username=guest,vers=1.0 0 0" | sudo tee -a /etc/fstab > /dev/null
 fi
 
-# Zip the logs and move them to the network drive
-log_file_name=$(hostname)_$(date +'%Y%m%d_%H%M%S').zip
-zip -r $log_file_name /var/log/* -x "/var/log/$local_folder/*"
-mv $log_file_name /mnt/network_drive/$local_folder/
+# Check if the local_folder exists in the network drive, if not create it
+if [ ! -d "/mnt/$local_folder/$local_folder" ]; then
+    echo "${yellow}Creating $local_folder in the network drive...${reset}"
+    sudo mkdir "/mnt/$local_folder/$local_folder"
+fi
 
-# Delete logs older than 6 months
-find /mnt/network_drive/$local_folder/ -name "*.zip" -mtime +180 -exec rm {} \;
-
-echo "${green}Backup completed successfully.${reset}"
+echo "${green}${bold}Setup completed.${reset}"
