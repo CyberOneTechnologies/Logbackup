1eef98570f6aab509eec59ca9cd298cb88e45479
tobyarnett <39145409+tobyarnett@users.noreply.github.com>
2023-05-21 00:37:56 -0500

    Update logbackup.sh

--- logbackup.sh
+++ logbackup.sh
@@ -10,47 +10,45 @@
 
 
 
-
-# Set color variables
+# Set color variables for better readability
 green=$(tput setaf 2)
 yellow=$(tput setaf 3)
-red=$(tput setaf 1)
+blue=$(tput setaf 4)
+reverse=$(tput rev)
 bold=$(tput bold)
 reset=$(tput sgr0)
 
-echo "${yellow}What is the network share drive? (i.e. //192.168.1.10/logs):${reset}"
+# Ask for the network drive
+echo "${blue}What is the network share drive? (i.e. //192.168.1.10/logs):${reset}"
 read network_drive
-echo "${yellow}What is the name of the local folder to use:${reset}"
+
+# Ask for the local folder name
+echo "${blue}What is the name of the local folder to use:${reset}"
 read local_folder
 
-# Create the mount point if it doesn't exist
-if [ ! -d "/mnt/$local_folder" ]; then
-    echo "${yellow}Creating mount point /mnt/$local_folder...${reset}"
-    sudo mkdir /mnt/$local_folder
+# Creating the local folder under /mnt
+echo "Creating local folder..."
+sudo mkdir -p /mnt/${local_folder}
+
+# Mount the network drive to the local folder
+echo "Mounting the network drive..."
+sudo mount -t cifs ${network_drive} /mnt/${local_folder}
+
+if [ $? -ne 0 ]; then
+  echo "${red}Failed to mount the network drive. Please check your settings and try again.${reset}"
+  exit 1
 fi
 
-# Mount the network drive
-echo "${yellow}Mounting the network drive...${reset}"
-mount_output=$(sudo mount -t cifs $network_drive /mnt/$local_folder -o username=guest,vers=1.0 2>&1)
+# Zip the log files and move them to the network drive
+echo "Creating and moving the log backup..."
+server_name=$(hostname)
+date_time=$(date '+%Y-%m-%d_%H-%M-%S')
+sudo tar -czf /var/log/${server_name}_${date_time}.tar.gz /var/log
+sudo mv /var/log/${server_name}_${date_time}.tar.gz /mnt/${local_folder}
 
-if [ $? -eq 0 ]; then
-    echo "${green}Network drive mounted successfully.${reset}"
-else
-    echo "${red}Failed to mount the network drive. Please check your settings and try again.${reset}"
-    echo "Error details: $mount_output"
-    exit 1
-fi
+# Delete old backups (> 180 days)
+echo "Deleting old backups..."
+sudo find /mnt/${local_folder} -name "*.tar.gz" -type f -mtime +180 -delete
 
-# Add the mount to /etc/fstab if it's not already there
-if ! grep -q "$network_drive /mnt/$local_folder" /etc/fstab; then
-    echo "${yellow}Adding the mount to /etc/fstab...${reset}"
-    echo "$network_drive /mnt/$local_folder cifs username=guest,vers=1.0 0 0" | sudo tee -a /etc/fstab > /dev/null
-fi
-
-# Check if the local_folder exists in the network drive, if not create it
-if [ ! -d "/mnt/$local_folder/$local_folder" ]; then
-    echo "${yellow}Creating $local_folder in the network drive...${reset}"
-    sudo mkdir "/mnt/$local_folder/$local_folder"
-fi
-
-echo "${green}${bold}Setup completed.${reset}"
+# Notify user of successful backup
+echo "${green}Backup process completed.${reset}"
